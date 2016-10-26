a5d38b891ead3334f373924040e11d2872844610
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-10-26 18:58:42 -0400

    Update processesTree.sh

--- processesTree.sh
+++ processesTree.sh
@@ -2,9 +2,11 @@
 
 #Question 1 for Assignment 2
 #Draws a tree of the active processes showing their parent until it reaches the init daemon [systemd]
+#Just brainstorming, i got all the commands right, just need to write a code with them now
+#Feel free to use it to do your own code if you have time to before me!!!
 
-#for each process
-#name
+#for each active process:
+#name:
 #print PID:
 #PPID:
 #STARTED:
@@ -15,13 +17,11 @@
 #til it reaches init=1 and PPID=0
 # --forest to construct tree
 
-
+#or for constructing tree:
 #for each i in ps list
 #for each pid in i
-# do pstree
-#ps needs pid
+#pstree
 #pstree displays processes in tree format startin from init
 #-p shows pid
 #pid of current bash is $$
-
-#-npu (sort by number pid, display owner
+#-npu (sort pid by number, display owner
