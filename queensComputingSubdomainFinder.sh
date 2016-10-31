c06cf69e057b8977f73dd4a2728d4a4c1e01a57e
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-10-31 19:05:46 -0400

    Update queensComputingSubdomainFinder.sh
    Question 4

--- queensComputingSubdomainFinder.sh
+++ queensComputingSubdomainFinder.sh
@@ -4,20 +4,21 @@
 #Question 4 for Assignment 2
 #Loops through Queen's IP addresses starting from 130.15.0.0 until 130.15.255.255 and prints out all the IP addresses along with the names of all cs.queensu.ca subdomains
 
-IP_file="/tmp/ip_file" #temporary file with IP addresses
-SD_file="/tmp/subdomain" #temporary file with IP's subdomains
+#Writes onto/creates files to store the IPS and subdomains
+echo "IP Addresses" >> IP_file.txt
+echo "Subdomains" >> SD_file.txt
+echo "nslookup output" >> output_file.txt
 
-echo "IP Address" >> IP_file
-echo "Subdomains" >> SD_file
-
-for ip in 130.15.{0..255}.{0.255}
+#Loops through range of ips
+for ip in 130.15.{0..255}.{0..255}
 do
-	echo ip >> IP_file ; @nslookup ip | echo >> SD_file
+	#Adds each IP to IP file and each corresponding subdomain to subdomain file
+	echo $ip >> IP_file.txt ; nslookup $ip >> output_file.txt
+	lineNumber=$(grep -n $ip output_file.txt)" #finds IP in the output file and gets the line number
+	#reads the line, prints second column (separated by tab) of the line (name = ...), prints the name of the subdomain, adds subdomain to subdomain file
+	sed '$lineNumber1;d' output_file.txt | awk -F "\t" '{print $2}' | awk -F " " '{print $3}' >> SD_file
 done
 
-#paste command adds IP and Domain files together and prints them separated by TABs
-paste IP_file SD_file ##why is it not recognizing the paste command???
+#Adds IP and Domain files together and prints them separated by TABs
+paste IP_file.txt SD_file.txt ##why is it not recognizing the paste command???
 
-#remove temporary files
-rm "IP_file"
-rm "SD_file"
