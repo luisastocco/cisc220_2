7f55ae4d0080250b1204f921ec5af3b3f4e5c18a
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-10-31 23:44:50 -0400

    Update processesTree.sh

--- processesTree.sh
+++ processesTree.sh
@@ -2,30 +2,6 @@
 
 #Question 1 for Assignment 2
 #Draws a tree of the active processes showing their parent until it reaches the init daemon [systemd]
-#Just brainstorming, i got all the commands right, just need to write a code with them now
-#Feel free to use it to do your own code if you have time to before me!!!
 
-#for each active process:
-#name:
-#print PID:
-#PPID:
-#STARTED:
-#by:
-pstree > pstree.txt
-while read p; do
-   echo $p;# echo -n ps axo pid,ppid,start,user;
-
-done<pstree.txt
-#ps axo p,ppid,start,user
-# --sort PID
-#til it reaches init=1 and PPID=0
-# --forest to construct tree
-
-#or for constructing tree:
-#for each i in ps list
-#for each pid in i
-#pstree
-#pstree displays processes in tree format startin from init
-#-p shows pid
-#pid of current bash is $$
-#-npu (sort pid by number, display owner
+#Views processes running on the system owned by any user and outputs the ip, parent ip, start time and who owns it, in a tree form, sorted by descending pid (since init=1 and all others=0, it starts at the init)
+ps axo pid,ppid,time,user --forest --sort=-pid #we couldn't figure out how to format it to print the ex[ected words befoe each piece of information
