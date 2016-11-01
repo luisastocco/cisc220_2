9a0c440c25f4166ec043505d0515298f07f35c9a
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-10-31 23:46:35 -0400

    done

--- processesTree.sh
+++ processesTree.sh
@@ -4,4 +4,4 @@
 #Draws a tree of the active processes showing their parent until it reaches the init daemon [systemd]
 
 #Views processes running on the system owned by any user and outputs the ip, parent ip, start time and who owns it, in a tree form, sorted by descending pid (since init=1 and all others=0, it starts at the init)
-ps axo pid,ppid,time,user --forest --sort=-pid #we couldn't figure out how to format it to print the ex[ected words befoe each piece of information
+ps axo pid,ppid,time,user --forest --sort=-pid #we couldn't figure out how to format it to print the expected words befoe each piece of information
